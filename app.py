2c2d2d5d66f409c774cea745461716e04bfaafe4
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-23 15:13:16 -0300

    Creating simple API with flask

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.get('/sum/<int:a>/<int:b>')
+def sum(a, b):
+    return {'result': a + b}, 200
+
+@app.get('/subtract/<int:a>/<int:b>')
+def subtract(a, b):
+    return {'result': a - b}, 200
+
+@app.get('/multiply/<int:a>/<int:b>')
+def multiply(a, b):
+    return {'result': a * b}, 200
+
+@app.get('/divide/<int:a>/<int:b>')
+def divide(a, b):
+    return {'result': a / b}, 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
