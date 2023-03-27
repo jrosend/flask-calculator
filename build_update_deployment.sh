a03c94caf069498bbb62557c275538fdb4382d6b
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-27 11:28:10 -0300

    Auto updating image tag on build

--- build_update_deployment.sh
+++ build_update_deployment.sh
@@ -2,8 +2,18 @@
 
 imageTag="study/flask-calculator:0.0.7"
 
-minikube image build -t "$imageTag" .
+imageTag=$(yq -er ".spec.template.spec.containers[0].image" < deployment.yaml)
 
-yq -yi --arg imageTag "$imageTag" '.spec.template.spec.containers[0].image = $imageTag' deployment.yaml
+IFS=':' read -ra tagComponents <<< "$imageTag"
+tag=${tagComponents[1]}
+IFS='.' read -ra versionComponents <<< "$tag"
+majorMinor="${versionComponents[0]}.${versionComponents[1]}"
+patch=$((versionComponents[2]+1))
+newTag="$majorMinor.$patch"
+newImageTag="${tagComponents[0]}:$newTag"
+
+minikube image build -t "$newImageTag" .
 
 kubectl apply -f deployment.yaml
+
+yq -yi --arg newImageTag "$newImageTag" '.spec.template.spec.containers[0].image = $newImageTag' deployment.yaml
