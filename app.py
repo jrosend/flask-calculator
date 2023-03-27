004295cee1abf629e62111fe7071467de2a1ea54
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-27 14:30:32 -0300

    Logging endpoints.

--- app.py
+++ app.py
@@ -3,24 +3,39 @@
 from flask import Flask
 import logging as log
 
-log.basicConfig(format='%(asctime)s %(clientip)-15s %(user)-8s %(message)s')
+log.basicConfig(format='%(asctime)s %(message)s')
 
 app=Flask(__name__)
 
 @app.get('/sum/<int:a>/<int:b>')
 def sum(a, b):
     log.info(f'{a} + {b}')
-    return {'result': a + b}, 200
+    return {
+        'operation': 'sum',
+        'a': a,
+        'b': b,
+        'result': a + b
+    }, 200
 
 @app.get('/subtract/<int:a>/<int:b>')
 def subtract(a, b):
     log.info(f'{a} / {b}')
-    return {'result': a - b}, 200
+    return {
+        'operation': 'subtract',
+        'a': a,
+        'b': b,
+        'result': a - b
+    }, 200
 
 @app.get('/multiply/<int:a>/<int:b>')
 def multiply(a, b):
     log.info(f'{a} * {b}')
-    return {'result': a * b}, 200
+    return {
+        'operation': 'multiply',
+        'a': a,
+        'b': b,
+        'result': a * b
+    }, 200
 
 @app.get('/divide/<int:a>/<int:b>')
 def divide(a, b):
@@ -28,11 +43,18 @@ def divide(a, b):
     if b == 0:
         log.error('Can\'t divide by 0')
         return {'error': 'Can\'t divide by 0'}, 400
-    return {'result': a / b}, 200
+    return {
+        'operation': 'divide',
+        'a': a,
+        'b': b,
+        'result': a / b
+    }, 200
 
 @app.get('/health')
 def health():
-    return {'status':'up'}, 200
+    return {
+        'status':'up'
+    }, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
