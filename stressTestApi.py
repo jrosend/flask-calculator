7eeeb43a0a3e11877d9069006fb0d82eb3e24e45
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-27 14:31:40 -0300

    Async stress test.

--- stressTestApi.py
+++ stressTestApi.py
@@ -2,15 +2,24 @@
 
 import requests
 import random
+import aiohttp
+import asyncio
 
 
 requestsNum = 1000
 
 operations = ['sum', 'subtract', 'multiply', 'divide']
 
-for i in range(0, requestsNum):
-    operation = operations[random.randint(0, len(operations)-1)]
-    randomA = random.randint(0, 1000)
-    randomB = random.randint(1, 1000)
-    response = requests.get(f'http://127.0.0.1:37783/{operation}/{randomA}/{randomB}')
-    print(response.json())
+requestsList = [
+    f'http://127.0.0.1:8001/{operations[random.randint(0, len(operations)-1)]}/{random.randint(0, 1000)}/{random.randint(0, 1000)}'
+    for _ in range(requestsNum)
+]
+
+async def stressTest():
+    async with aiohttp.ClientSession() as session:
+        for request in requestsList:
+            async with session.get(request) as resp:
+                response = await resp.json()
+                print(response)
+
+asyncio.run(stressTest())
