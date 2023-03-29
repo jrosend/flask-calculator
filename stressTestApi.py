d2fa4932524b74de4d00234ebfb61419e5ee5074
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-29 00:15:14 -0300

    Enhancing stress test script

--- stressTestApi.py
+++ stressTestApi.py
@@ -1,25 +1,38 @@
 #!/bin/env python
 
-import requests
 import random
-import aiohttp
 import asyncio
+import time
+import time
+import logging as log
+
+from aiohttp import ClientSession
+
+log.basicConfig(format='%(asctime)s %(message)s')
 
 
-requestsNum = 1000
+requestBatchCount = 20
+requestsCount = 500
+
+print(f'Sending {requestBatchCount} batches of {requestsCount} requests each')
 
 operations = ['sum', 'subtract', 'multiply', 'divide']
 
-requestsList = [
-    f'http://127.0.0.1:8001/{operations[random.randint(0, len(operations)-1)]}/{random.randint(0, 1000)}/{random.randint(0, 1000)}'
-    for _ in range(requestsNum)
-]
-
-async def stressTest():
-    async with aiohttp.ClientSession() as session:
+async def stressTest(requests):
+    async with ClientSession() as session:
         for request in requestsList:
             async with session.get(request) as resp:
                 response = await resp.json()
-                print(response)
+                log.debug(response)
 
-asyncio.run(stressTest())
+
+for i in range(0, requestBatchCount):
+    requestsList = [
+        f'http://127.0.0.1:8001/{operations[random.randint(0, len(operations)-1)]}/{random.randint(0, 1000)}/{random.randint(0, 1000)}'
+        for _ in range(requestsCount)
+    ]
+    print(f'{i+1}/{requestBatchCount}: Sending {len(requestsList)} requests...')
+    start_time = time.time()
+    asyncio.run(stressTest(requestsList))
+    seconds = time.time() - start_time
+    print('\tFinished in:', time.strftime("%H:%M:%S",time.gmtime(seconds)))
